939312eb14a4fe1791aa16d5f4252d5ae810ef55
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-12 00:00:12 +0530

    some change

--- app.js
+++ app.js
@@ -18,6 +18,42 @@ app.get('/', async (req, res) => {
     res.send("User created successfully!")
 })
 
+app.get('/users', async (req, res) => {
+    try {   
+        const users = await userModel.find();
+        res.json(users);
+    } catch(err) {
+        res.status(500).send("Error fetching your details")
+    }
+});
+
+
+
+//this will update through id
+app.put('/users/:id', async (req, res) => {
+    try {
+        const updatedUser = await userModel.findByIdAndUpdate(
+            req.params.id,
+            { username: "updatedName" },
+            { new: true }
+        );
+
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(500).send("Error updating user");
+    }
+});
+
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        await userModel.findByIdAndDelete(req.params.id);
+        res.send("User deleted");
+    } catch (err) {
+        res.status(500).send("Error deleting user");
+    }
+});
+
 
 // 'health' API
 app.get("/health", (req, res) => {
@@ -26,5 +62,5 @@ app.get("/health", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");``
+    console.log("Server is running on port 3000");
 })  //server is only created, but not running yet. to start we need to call listen method
