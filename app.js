da3763a302b04573d153940ebee8201ece7b4dfe
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-11 20:37:51 +0530

    some update

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-
 app.listen(3000, () => {
     console.log("Server is running on port 3000");``
-})  
+})  //server is only created, but not running yet. to start we need to call listen method
+
+// '/' API
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+// 'health' API
+app.get("/health", (req, res) => {
+    res.send("server is healthy!");
+})
+
+app.get("/health", (req, res) => {
+    res.send("server is healthy!");
+})
