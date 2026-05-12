edbdf35e1e1ed09179cdd85e59d27bfa8deadc43
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-12 14:51:28 +0530

    something

---
+++ Personal-finance-tracker/client/src/utils/api.transaction.js
@@ -0,0 +1,15 @@
+export const getTransactions = async () => {
+  return [];
+};
+
+export const createTransaction = async (data) => {
+  return data;
+};
+
+export const updateTransaction = async (id, data) => {
+  return data;
+};
+
+export const deleteTransaction = async (id) => {
+  return true;
+};
--- Personal-finance-tracker/server/index.js
+++ Personal-finance-tracker/server/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("API running"));
 app.use("/api/auth", authRoutes);  
+app.use("/api/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server`));
